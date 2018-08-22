eaee4278e49708260faa038bbeea72b591474223
AHMEDKHAN10 <40977160+AHMEDKHAN10@users.noreply.github.com>
2018-08-22 10:12:28 -0700

    Add files via upload

---
+++ PIR_TEMP_FAN.ino
@@ -0,0 +1,65 @@
+
+int motion = 2;
+int fan =3;
+#include <DHT.h>
+
+#define DHTPIN 6
+#define DHTTYPE DHT22
+DHT dht (DHTPIN, DHTTYPE);
+
+float hum;
+float temp;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(motion, INPUT);
+  pinMode(fan, OUTPUT);
+  dht.begin();
+}
+
+void loop() 
+{
+   hum = dht.readHumidity();
+   temp= dht.readTemperature();
+
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    
+  
+  long sensor = digitalRead(motion);
+  
+  if (sensor == HIGH && temp > 26) 
+    {
+    digitalWrite (fan, HIGH);
+    
+    Serial.println("Active");
+    delay (10000);
+    }
+  
+    else
+    {
+    digitalWrite(fan,LOW);
+    }
+  
+  
+
+  
+  
+  if (digitalRead(2)== HIGH)
+  {
+    Serial.println("Active");
+
+  }
+  else 
+  {
+    Serial.println("Inactive");
+
+  }
+  delay(500);
+}
+
+
